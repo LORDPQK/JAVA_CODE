d93d505c1ea13818f71b631bee3fedc2ca60340c
LORDPQK <62390591+LORDPQK@users.noreply.github.com>
2021-11-30 14:43:36 +0800

    Delete src directory

--- src/main/java/ocm/pqn/comtroller/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package ocm.pqn.comtroller.dao;
-
-import ocm.pqn.comtroller.entity.User;
-
-import java.util.List;
-
-public interface UserDAO {
-    List<User> queryUsers();
-}
--- src/main/java/ocm/pqn/comtroller/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package ocm.pqn.comtroller.service;
-
-import ocm.pqn.comtroller.entity.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> queryUsers();
-}
